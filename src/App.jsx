b84e54c89abab7cafc92254a3902f3c111320c5d
Anand <anand.work2005@gmail.com>
2025-08-03 12:38:32 +0530

    complete the navbar design and all

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className='container'>
-      <Navbar />
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,19 @@
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCartShopping, FaM } from "react-icons/fa6";
 import { GoSun } from "react-icons/go";
 import { FaMoon } from "react-icons/fa";
+import './Navbar.css';
 
-function Navbar() {
+function Navbar({ theme, setTheme }) {
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
+  const toggleIcon = theme === 'light' ? <FaMoon /> : <GoSun />
+
   return (
     <div className="navbar">
-      <div className="title">
-        <h1>Shopping Cart</h1>
-      </div>
+      <h1>Shopping Cart</h1>
 
       <ul className="links">
         <li>Home</li>
@@ -17,8 +23,7 @@ function Navbar() {
 
       <div className="cart-theme">
         <FaCartShopping />
-        <GoSun />
-        <FaMoon />
+        <p onClick={toggleTheme}>{toggleIcon}</p>
       </div>
     </div>
   )
