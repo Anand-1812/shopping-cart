c3d42967bede335312faa9a240e468233780f07a
Anand-1812 <anand.work2005@gmail.com>
2025-12-28 20:31:22 +0530

    fix some bugs

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NnuhpKyFCPGQcK77hjqFXGy0ydLGZIH+gxlKKGhPZ10=\"\n}"
--- .next/dev/static/chunks/app_(root)_layout_tsx_7f738c16._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a254e810._.js"
-],
-    source: "dynamic"
-});
